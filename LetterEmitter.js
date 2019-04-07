49b95c054432c0c817e5af824dd0ce0a0a2e3c93
ollinieminen <otn987@gmail.com>
2019-04-07 16:40:56 -0700

    fixed code so that spaces wont emmit a letter

--- LetterEmitter.js
+++ LetterEmitter.js
@@ -1,22 +1,28 @@
 const { EventEmitter } = require('events');
 
-class LetterEmitter extends EventEmitter{
-    constructor(){
+class LetterEmitter extends EventEmitter 
+{
+    constructor() 
+    {
         super();//needed to run constructor when extend....
     }
-  
-    read(str){
+
+    read(str) 
+    {
         //instead of letters you have str
-        str.split('').forEach((letterOfString, i) =>{
-            //ee.email you have emit as one of your (this methods)      
-          
-            const data = 
+        const pattern =/[a-z]/i;
+        str.split('').forEach((letterOfString, i) => 
+        {
+            if(pattern.test(letterOfString))
             {
-                letter: letterOfString,
-                offset: i
-            };
-            this.emit('letter', data);
-        });  
+                const data =
+                {
+                    letter: letterOfString,
+                    offset: i
+                };
+                this.emit('letter', data);
+            }            
+        });
         this.emit('end');
     }
 };
--- LetterEmitter.test.js
+++ LetterEmitter.test.js
@@ -2,27 +2,34 @@ const LetterEmitter = require('./LetterEmitter');
 
 
 
-describe('LetterEmitterTest', () => {
+describe('LetterEmitterTest', () => 
+{
     let letterEmitter;
-    beforeEach(() => {
+    beforeEach(() => 
+    {
         letterEmitter = new LetterEmitter;
     })
-    it('splits a string and emmits an event for each letter', done => {
-        const string = 'abc';
+    it('splits a string and emmits an event for each letter', done => 
+    {
+        const string = 'aBc hello duck';
         const pattern = /[a/z]/i;
         const letterMockHandler = jest.fn();
 
         letterEmitter.on('letter', letterMockHandler);
 
-        letterEmitter.once('end', () => {
-            expect(letterMockHandler).toHaveBeenCalledTimes(3);
-
-            [...string].forEach((letter, offset) => {
-                expect(letterMockHandler).toHaveBeenCalledWith(
-                    {
-                        letter,
-                        offset
-                    });
+        letterEmitter.once('end', () => 
+        {
+            expect(letterMockHandler).toHaveBeenCalledTimes(12);
+            [...string].forEach((letter, offset) => 
+            {
+                if(pattern.test(letter))
+                {
+                    expect(letterMockHandler).toHaveBeenCalledWith(
+                        {
+                            letter,
+                            offset
+                        });
+                }        
             })
         })
         letterEmitter.read(string);
